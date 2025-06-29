f91eb40a4f679da68dfbfb1a65199343269f25e7
Rajith Karunarathne <rajithkarunarathne@gmail.com>
2025-06-29 10:23:25 +0530

    std import new

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$con = mysqli_connect("localhost", "root", "123456", "student_management");
+// if($con){
+//     echo "successfull";
+// }else{
+//     echo "Not successfull";
+// }
+
+?>
